c1486f5bc72a98ed405155c9699c89e85e88e533
Ionut Rosan <ionut.rosan@nordlogic.com>
2021-10-11 20:16:47 +0300

    TASK-1 Added env variables

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
-  const port = 3000;
-
-
   const app = await NestFactory.create(AppModule);
+  
+
+  if (process.env.NODE_ENV === 'development') {
+    app.use(cors);
+  }
+
+  const serverConfig = config.get('server');
+
+  const port = process.env.PORT || serverConfig.port;
+
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
 }
